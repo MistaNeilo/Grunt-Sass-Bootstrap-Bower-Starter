acf4ccc9484ab5ee2fe62e5e80d98335cf349163
unknown <neilk01@LHZ13M32.live.picklesauctions.com.au>
2016-02-01 09:46:09 +1100

    updates to build

--- Grunt.js
+++ Grunt.js
@@ -1,35 +1,70 @@
-module.exports = function (grunt) {
+(function () {
+    'use strict';
+}());
 
+module.exports = function (grunt) {
     grunt.initConfig({
-        jshint: {
-            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
             options: {
-                globals: {
-                    jQuery: true
+                separator: 'rnrn'
+            },
+            js: {
+                src: ['src/main.js'],
+                dest: 'dist/basic.js',
+            }
+            //            css: {
+            //                src: ['sass/*.css', 'src/extras.js'],
+            //                dest: 'dist/with_extras.js',
+            //            }
+        },
+
+        uglify: {
+            options: {
+                banner: '/*! &lt;%= pkg.name %&gt; &lt;%= grunt.template.today("dd-mm-yyyy") %&gt; */n'
+            },
+            dist: {
+                files: {
+                    'js/main.min.js': ['&lt;%= concat.js.dest %&gt;']
                 }
             }
         },
-        watch: {
-            compass: {
-                files: ['**/*.{scss,sass}'],
-                tasks: ['compass:dev']
-            }
-            uglify: {
-                files: ['<%= jshint.files %>'],
-                tasks: ['uglify']
-            }
 
+        jshint: {
+            files: ['gruntfile.js', 'js/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        },
+
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'sass/',
+                    cssDir: 'css/',
+                    environment: 'development',
+                    outputStyle: 'compressed'
+                }
+            }
+        },
+
+        watch: {
+            files: ['&lt;%= jshint.files %&gt;', 'sass/**/*.scss'],
+            tasks: ['concat', 'uglify', 'jshint', 'compass']
         }
+
     });
 
-
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['jshint', 'concat', 'copy', 'uglify', 'compass', 'watch']);
-
+    grunt.registerTask('default', ['concat', 'uglify', 'jshint', 'compass', 'watch']);
 };
